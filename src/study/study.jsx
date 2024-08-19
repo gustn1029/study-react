0cb5adf87c2232f31d9b734f04557029d3971430
hs0820 <gustn1029@gmail.com>
2024-08-19 16:46:26 +0900

    리액트 강의 study 초기화

--- src/study/study.jsx
+++ src/study/study.jsx
@@ -1,25 +1,10 @@
-import ColorText from "./ColorText";
-import DateComponent from "./DateComponent";
-import Fragments from "./Fragments";
-import ListComponent from "./ListComponent";
-import Resume from "./Resume";
 
 const Study = () => {
-  const items = [
-    { id: 1, name: 'Apple', desc: '빨간건 사과' },
-    { id: 2, name: 'Banana', desc: '바나나는 길어' },
-    { id: 3, name: 'Cherry', desc: '체리는 비싸' }
-];
-  return <div>
-    <h1>240814 강의</h1>
-    <DateComponent />
-    <ListComponent />
-    <Fragments data={items} />
-    <Resume hello={'안녕하세요'} name={"개리"} hobby={"게임"} food={"고기"} color={"blue"} />
-    <ColorText color="red" />
-    <ColorText color="green" />
-    <ColorText color="blue" />
-  </div>;
+  return (
+    <div>
+      <h1>240820 강의</h1>
+    </div>
+  );
 };
 
 export default Study;
